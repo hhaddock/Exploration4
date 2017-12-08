fe2b3568a4efd4bfd3a4b08ffb8de97f273b24ae
Hayden Haddock <Hayden Haddock>
2017-12-08 17:11:24 -0600

    Working on UI and functionality

--- js/main.js
+++ js/main.js
@@ -1,15 +1,41 @@
-window.onload = function(){
+$(document).ready(function(){
   var app = new Vue({
     el: '#app',
     data: {
-      title: '',
-      desc: '',
-      date: ''
+      todo: {
+        title: '',
+        desc: '',
+        date: '',
+      },
+      ToDos: []
     },
     methods: {
+      getTodos: function() {
+        this.ToDos = (localStorage.getItem('Todo') !== null) ? JSON.parse(localStorage.getItem('Todo')) : [];
+        console.log(this.ToDos)
+      },
+
       addTodo: function() {
-        console.log("Title:" + this.title + " Desc: " + this.desc + " Date: " + this.date);
+        this.ToDos.push(this.todo);
+        console.log(this.ToDos)
+        localStorage.setItem('Todo', JSON.stringify(this.ToDos));
+        this.getTodos();
+        this.clearForm();
+      },
+
+      removeTodo: function() {
+
+      },
+
+      clearForm: function() {
+        this.todo.title = '';
+        this.todo.desc = '';
+        this.todo.date = '';
       }
+    },
+
+    beforeMount(){
+      this.getTodos();
     }
   })
-}
+})
